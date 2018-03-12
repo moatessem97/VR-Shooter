2cd1acf2de82169f94f55a7992dab679e169b6cb
Moatassem Waly <m.waly@saedubai.com>
2018-03-12 01:41:46 -0700

    P1
    P1

---
+++ Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour {
+
+	public Transform Cell;
+	public float speed;
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, Cell.position, step);
+	}
+}
